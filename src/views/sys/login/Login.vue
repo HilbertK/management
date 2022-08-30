31b38fd75d4cf6a461f35729d76a27eb9227eadb
wangmingke <906932759@qq.com>
2022-08-30 17:45:11 +0800

    feat: login style

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -1,20 +1,10 @@
 <template>
-  <div :class="prefixCls" class="relative w-full h-full px-4">
-    <!-- <AppLocalePicker class="absolute text-white top-4 right-4 enter-x xl:text-gray-600" :showText="false" v-if="!sessionTimeout && showLocale" /> -->
-    <!-- <AppDarkModeToggle class="absolute top-3 right-7 enter-x" v-if="!sessionTimeout" /> -->
-    <span class="-enter-x xl:hidden">
-      <AppLogo :alwaysShowTitle="true" />
-    </span>
-
+  <div :class="prefixCls" class="relative w-full h-full">
     <div class="container relative h-full py-2 mx-auto sm:px-10">
       <div class="flex h-full">
         <div class="hidden min-h-full pl-4 mr-4 xl:flex xl:flex-col xl:w-6/12">
-          <AppLogo class="-enter-x" />
           <div class="my-auto">
-            <img :alt="title" src="../../../assets/svg/login-box-bg.svg" class="w-1/2 -mt-16 -enter-x" />
-            <div class="mt-10 font-medium text-white -enter-x">
-              <span class="inline-block mt-4 text-3xl"> {{ t('sys.login.signInTitle') }}</span>
-            </div>
+            <div class="mt-10 font-medium text-white -enter-x"></div>
           </div>
         </div>
         <div class="flex w-full h-full py-5 xl:h-auto xl:py-0 xl:my-0 xl:w-6/12">
@@ -32,7 +22,6 @@
 </template>
 <script lang="ts" setup>
   import { computed } from 'vue';
-  import { AppLogo } from '/@/components/Application';
   import LoginForm from './LoginForm.vue';
   import RegisterForm from './RegisterForm.vue';
   import { useGlobSetting } from '/@/hooks/setting';
@@ -112,9 +101,8 @@
       left: 0;
       width: 100%;
       height: 100%;
-      margin-left: -48%;
-      background-image: url(/@/assets/svg/login-bg.svg);
-      background-position: 100%;
+      background-image: url(/@/assets/svg/login-bg.png);
+      background-position: left top;
       background-repeat: no-repeat;
       background-size: auto 100%;
       content: '';
